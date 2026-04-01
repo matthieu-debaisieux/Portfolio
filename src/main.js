f361ea268c55a9326b0e94f13d64764707e3632a
Amiio974 <matthieu.debaisieux@gmail.com>
2026-04-01 17:54:05 +0200

    début JS
    utilisation anime.js sur l'Acceuil

--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
 import '../Styles/style.css'
-import { animate } from 'animejs'
+import { createTimeline, stagger, splitText } from 'animejs';
+
+const { chars } = splitText('h1', {
+    chars: {
+        wrap: 'clip',
+        clone: 'bottom'
+    },
+});
+
+createTimeline()
+.add(chars, {
+    y: '-115%',
+    loop: true,
+    loopDelay: 5500,
+    duration: 750,
+    ease: 'inOut(2)',
+}, stagger(150, { from: 'center' }));
